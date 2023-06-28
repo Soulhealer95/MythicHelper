21cd08e43e0ec02dbaa715ef3030b2df6ea5efcf
Shivam Saxena <sstshivam1@hotmail.com>
2023-06-27 22:58:22 -0400

    Discord Bot Working

--- Wrapper/RaiderAPI.rb
+++ Wrapper/RaiderAPI.rb
@@ -6,6 +6,7 @@ class RaiderAPI < Request
     super
     @supported_region = "us"
     @current_season = "9"
+    @current_dungeons = []
 
     # character specific
     @base_uri = "https://raider.io/api/v1/characters/profile?region=" + \
@@ -18,20 +19,86 @@ class RaiderAPI < Request
 
   end
 
-  def FieldData(field)
+  def MythicCurrAffixes
+    url = @mythic_url + "affixes?region=" + @supported_region + "&locale=en"
+    out = []
+    data = getData(url)
+    for item in data["affix_details"]
+      out.push(item["name"])
+    end
+    return out
+  end
 
+  def MythicRating
+    data = FieldData("mythic_plus_scores_by_season:current")
+    ratings = data[0]["scores"]
+    main_rating = ratings["all"]
+    return main_rating
+  end
+  
+  def MythicRuns
+    runs = AllRuns()
+    return runs
+    out = {}
+    return out if !runs
+  end
+
+  private
+  # given a hash {"Fortified" => x1, "Tyrannical" => x2}
+  # replace with rating using following method by Blizzard
+  # max_val * 1.5 + min_val * 0.5
+  # return hash with rating 
+  def calculate_rating(data)
+    fort = "Fortified"
+    tyr = "Tyrannical"
+    data[tyr] = [0,0] if !data[tyr]
+    data[fort] = [0,0] if !data[fort]
+    out = { fort => [0, 0], tyr => [0, 0] } 
+    if data[fort][0] > data[tyr][0]
+      out[fort] = [ data[fort][0] * 1.5, data[fort][1] ]
+      out[tyr]  = [ data[tyr][0] * 0.5, data[tyr][1] ]
+    else
+      out[tyr] =  [ data[tyr][0] * 1.5, data[tyr][1] ]
+      out[fort]  = [ data[fort][0] * 0.5, data[fort][1] ]
+    end
+    out[fort][0] = out[fort][0].round(1)
+    out[tyr][0] = out[tyr][0].round(1)
+    return out
+  end
+
+  def BestRun
+    data = FieldData("mythic_plus_best_runs")
+    return OrganizeDungeons(data) 
+  end
+
+  # Best run but in alternate ones
+  def AltRun
+    data = FieldData("mythic_plus_alternate_runs")
+    return OrganizeDungeons(data) 
+  end
+
+  # Combined Runs for Current Dungeons
+  def AllRuns
+    best = BestRun()
+    alt = AltRun()
+    return nil if !best && !alt
+    return best if !alt
+    return SortRuns(best, alt)
+  end
+
+  # Following make API Calls to get data
+  def FieldData(field)
     url = @params_url + field
     data = getData(url)
     return nil if !data
-    @current_dungeons = []
 
     # In case there are some  special characters in the params
-    format_field= (field.split(":")[0])
+    format_field = (field.split(":")[0])
     return data[format_field]
   end
 
   def Current_Dungeons
-    if @current_dungeons && @current_dungeons.empty?
+    if @current_dungeons.empty? 
       url = @mythic_static + @current_season
       data = getData(url)["seasons"][0]["dungeons"]
       for i in data
@@ -41,75 +108,39 @@ class RaiderAPI < Request
     return @current_dungeons
   end
 
-  #  just return {dungeon name: {affix : {level, score}}}
+  def SortRuns(best, alt)
+    dungeons = Current_Dungeons()
+    input = nil
+    out = {}
+
+    for dung in dungeons
+      if alt[dung] != nil
+        input = best[dung].merge(alt[dung]) 
+      elsif best[dung] != nil
+        input = best[dung]
+      else
+        input = nil
+      end
+      out[dung] = input
+
+      #convert score to rating
+      out[dung] = calculate_rating(out[dung]) if input
+    end
+
+    return out
+  end
+
+  #  just return {dungeon name: {affix : [score, level]}}
   def OrganizeDungeons(data)
+    return nil if !data || data.empty?
+
     output = {}
     for dungeons in data
-      output[dungeons["dungeon"]] = { dungeons["affixes"][0]["name"] =>  dungeons["score"]  }
+      output[dungeons["dungeon"]] = { dungeons["affixes"][0]["name"] =>  [ dungeons["score"], dungeons["mythic_level"] ]  }
     end
     return output
   end
 
-  def MythicRating
-    data = FieldData("mythic_plus_scores_by_season:current")
-    ratings = data[0]["scores"]
-    main_rating = ratings["all"]
-    return main_rating
-  end
-
-  def BestRun
-    data = FieldData("mythic_plus_best_runs")
-    return OrganizeDungeons(data) if (data && data != {})
-    return nil
-  end
-
-  # Best run but in alternate ones
-  def AltRun
-    data = FieldData("mythic_plus_alternate_runs")
-    return OrganizeDungeons(data) if (data && data != {})
-    return nil
-  end
-
-  # Combined Runs for Current Dungeons
-  def AllRuns
-    best = BestRun()
-    alt = AltRun()
-    return nil if !best && !alt
-    return best if !alt
-    return alt if !best
-    puts alt 
-    puts best
-
-    dungeons = Current_Dungeons()
-    out = {}
-    for dung in dungeons
-      out[dung] = best[dung].merge(alt[dung])
-
-      #convert score to rating
-      out[dung] = calculate_rating(out[dung])
-    end
-    return out
-  end
-
-  # given a hash {"Fortified" => x1, "Tyrannical" => x2}
-  # replace with rating using following method by Blizzard
-  # max_val * 1.5 + min_val * 0.5
-  # return hash with rating 
-  def calculate_rating(data)
-    fort = "Fortified"
-    tyr = "Tyrannical"
-    out = { fort => nil, tyr => nil } 
-    if data[fort] > data[tyr]
-      out[fort] = data[fort] * 1.5
-      out[tyr]  = data[tyr] * 0.5
-    else
-      out[tyr] = data[tyr] * 1.5
-      out[fort]  = data[fort] * 0.5
-    end
-    out[fort] = out[fort].round(1)
-    out[tyr] = out[tyr].round(1)
-    return out
-  end
 
 end
 
--- Wrapper/use.rb
+++ Wrapper/use.rb
@@ -1,18 +1,119 @@
 require_relative "RaiderAPI"
-require_relative "Blizzard"
 require_relative "mythicdb"
 require_relative "DBAPI"
+require 'discordrb'
 
 
+class MythicBot
+  def initialize(token)
+    @token = token
+    @bot = Discordrb::Commands::CommandBot.new token: @token, prefix: '!'
+    @db = mythicdb.new
+    setupCommands()
+    @bot.run
+  end
+  def setupCommands
+    # M+ Rating
+    @bot.command(:rating,  min_args: 2, max_args: 2, description: 'Get your Mythic+ Rating.', usage: 'rating [charactername] [realm]') do |_event, username, realm|
+      rating = UpdateRating(username, realm)
+      return "No Rating Found" if !rating
+      return rating
+    end
+
+    # Best Runs
+    @bot.command(:runs,  min_args: 2, max_args: 2, description: 'Get your M+ Runs', usage: 'runs [charactername] [realm]') do |_event, username, realm|
+      raid = RaiderAPI.new(username, realm)
+      return PrettyPrintRuns(raid.MythicRuns)
+    end
+
+    # Next Best
+    @bot.command(:nextbest,  min_args: 2, max_args: 2, description: 'Best Dungeon to Run Next', usage: 'nextbest [charactername] [realm]') do |_event, username, realm|
+      raid = RaiderAPI.new(username, realm)
+      return NextBest(raid)
+    end
+
+    # App Rating
+    @bot.command(:apprank,  min_args: 2, max_args: 2, description: 'Server Ranking among Bot Users', usage: 'apprank [charactername] [realm]') do |_event, username, realm|
+      if UpdateRating(username, realm)
+        rank = @db.get_app_rank(username, realm)
+      else
+        rank = "No Rating Found"
+      end
+      return rank
+    end
+
+
+  end
+
+  private
+  def UpdateRating(username, realm)
+      raid = RaiderAPI.new(username,realm)
+      rating = raid.MythicRating
+      if rating
+        @db.add_rating(username, realm, rating)
+        return rating
+      end
+      return nil
+  end
+  def NextBest(raid)
+    min = [999, ""]
+
+    affix = (raid.MythicCurrAffixes)[0].to_s
+    data = raid.MythicRuns
+
+    data.each do |key, val|
+      if val[affix][0] == 0
+        min[0] = "Any level"
+        min[1] = key
+        break
+      end
+
+      if val[affix][0] < min[0]
+        min[0] = val[affix][0]
+        min[1] = key
+      end
+
+    end
+    out = min[1] + " - "
+    if min[0] == "Any level"
+      out = out + min[0] 
+    else
+      out = out + (data[min[1]][affix][1] + 1).to_s
+    end
+    return out
+  end
+
+  def PrettyPrintRuns(data)
+    str = ""
+    data.each do |key, val|
+      str = str +  "#{key}:\nFortified (#{'%02d' % val["Fortified"][1]}) #{'%06.2f' % val["Fortified"][0]}   | |    Tyrannical (#{'%02d' % val["Tyrannical"][1]}) #{'%06.2f' % val["Tyrannical"][0]}\n"
+    end
+    return str
+  end
+
+end
+
+
+token = 'MTExMzYyOTY0NjA5ODAxNDMxOQ.GkhUBS.gmLHkY8rcXpansiDY5R468o99s36BNmekjDVJo'
+MythicBot.new(token)
+
+
+=begin
 realm = "Sargeras"
-username= "Brewaholic"
+#username= "Brewaholic"
+username= "ixys"
+realm = "Frostmourne"
+username = "Zyyna"
+
 raid = RaiderAPI.new(username, realm)
-out = raid.AllRuns
+#puts PrettyPrintRuns(raid.MythicRuns)
+#data = raid.MythicRuns
+puts NextBest(raid)
+
 
 
 #puts data["keystone_affixes"]
 #File.write("log.txt", out)
-=begin
 bliz = BlizzardAPI.new(username, realm)
 
 puts "Summary for " + username
