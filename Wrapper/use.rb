0b496d23eb021f5e10a063e52d9b8e502e432a53
Shivam Saxena <sstshivam1@hotmail.com>
2023-06-28 22:04:50 -0400

    changed name of DB

--- Wrapper/use.rb
+++ Wrapper/use.rb
@@ -13,7 +13,7 @@
 #  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 require_relative "RaiderAPI"
-require_relative "mythicdb"
+require_relative "MythicDB"
 require 'discordrb'
 
 
